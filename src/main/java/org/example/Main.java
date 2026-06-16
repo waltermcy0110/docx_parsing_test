adf21ab7b60a49efe2f76f5b6a151ac1c3d9d37f
waltermcy0110 <waltermcy0110@gmail.com>
2026-06-16 19:41:17 -0400

    removed duplicate loop; checkpoint before heavy modification to CTR section to accommodate parsing of images (maybe for each paragraph, start with getRuns instead, we're just extracting text atm)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,10 @@ package org.example;
 // https://mkyong.com/java/java-read-and-write-microsoft-word-with-apache-poi/
 
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.IBodyElement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlObject;
 import org.openxmlformats.schemas.officeDocument.x2006.math.CTOMath;
@@ -25,71 +27,99 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
+    static int count;
     XWPFDocument doc;
-
 
     public static void main(String[] args) throws IOException {
 
         String filename = "dummy_mcq.docx";
         System.out.printf("Parsing test. Sample MCQs with different formats (not exhaustive) included.");
 
-        try (XWPFDocument doc = new XWPFDocument(
+        try (XWPFDocument document = new XWPFDocument(
                 Files.newInputStream(Paths.get(filename)))) {
 
-            List<XWPFParagraph> list = doc.getParagraphs();
-            for (XWPFParagraph paragraph : list) {
+            List<XWPFParagraph> list = document.getParagraphs();
 
-                // skip empty paragraphs
-                if (paragraph.getText().trim().isEmpty()) continue;
-
-
-                System.out.println("\nParagraph: " + paragraph.getText());
-
-                // works as expected
-//                System.out.println("Underlying XML:");
-//                System.out.println(paragraph.getCTP().xmlText());
-
-                // parsing of word equations:
-                // gpt and https://stackoverflow.com/questions/44748712/reading-equations-formula-from-word-docx-to-html-and-save-database-using-jav
-                // https://developer.adobe.com/experience-manager/reference-materials/cloud-service/javadoc/org/openxmlformats/schemas/wordprocessingml/x2006/main/CTRow.html
+            // tables are not captured by XWPFParagraph
+            // online sources and https://blog.csdn.net/hwtwilight/article/details/151837532
+            for (IBodyElement element: document.getBodyElements()) {
+                // paragraphs: text, equations, etc
+                if (element instanceof XWPFParagraph) {
+                    // oh lmao i kept the original loop
 
 
+                    // for (XWPFParagraph paragraph: list) {
+                    XWPFParagraph paragraph = (XWPFParagraph) element; // just doing this for convenient not readability loll
 
-                // now iterate over each CTP object
-                XmlCursor cursor = paragraph.getCTP().newCursor();
-                // System.out.println("Type before toFirstChild: " + cursor.getName());
-                cursor.toFirstChild(); // goes down from paragraph to run? nope, to p prop for all occurrences
-                // System.out.println("Type after: " + cursor.getName());
-                do {
-                    XmlObject xmlobj = cursor.getObject();
-                    // System.out.println(xmlobj.xmlText());
+                    // skip empty paragraphs
+                    if (paragraph.getText().trim().isEmpty()) continue;
 
-                    XmlCursor.TokenType tokenType = cursor.currentTokenType();
-                    // System.out.println("Type: " + cursor.getName());
-                    // System.out.println("is run? " + (cursor.getObject() instanceof CTR));
-                    // runs showed up as non-runs, made me look into import statements
 
-                    // determine object type
-                    // text run
-                    if (xmlobj instanceof CTR) {
-                        for (CTText ctText: ((CTR) xmlobj).getTList()) {
-                            System.out.println("Text run: " + ctText.getStringValue());
+                    // System.out.println("\nParagraph: " + paragraph.getText());
+
+                    // works as expected
+//                        System.out.println("Underlying XML:");
+//                        System.out.println(paragraph.getCTP().xmlText());
+
+                    // parsing of word equations:
+                    // gpt and https://stackoverflow.com/questions/44748712/reading-equations-formula-from-word-docx-to-html-and-save-database-using-jav
+                    // https://developer.adobe.com/experience-manager/reference-materials/cloud-service/javadoc/org/openxmlformats/schemas/wordprocessingml/x2006/main/CTRow.html
+
+
+
+                    // now iterate over each CTP object
+                    XmlCursor cursor = paragraph.getCTP().newCursor();
+                    // System.out.println("Type before toFirstChild: " + cursor.getName());
+                    cursor.toFirstChild(); // goes down from paragraph to run? nope, to p prop for all occurrences
+                    // System.out.println("Type after: " + cursor.getName());
+                    do {
+                        XmlObject xmlobj = cursor.getObject();
+                        // System.out.println(xmlobj.xmlText());
+
+                        XmlCursor.TokenType tokenType = cursor.currentTokenType();
+                        System.out.println("Type: " + cursor.getName());
+                        // System.out.println("is run? " + (cursor.getObject() instanceof CTR));
+                        // runs showed up as non-runs, made me look into import statements
+
+                        // determine object type
+                        // text run
+                        if (xmlobj instanceof CTR) {
+                            for (CTText ctText: ((CTR) xmlobj).getTList()) {
+                                System.out.println("Text run: " + ctText.getStringValue());
+                            }
+
+                            // TODO: handle images since images are inside runs
                         }
-                    }
-                    // math (idk if inline or block)
-                    else if (xmlobj instanceof CTOMath) {
-                        System.out.println("Equation: " + xmlobj.xmlText());
-                    }
+                        // math (idk if inline or block)
+                        else if (xmlobj instanceof CTOMath) {
+                            System.out.println("Equation: " + xmlobj.xmlText());
+                        }
 
-                } while (cursor.toNextSibling());
+                    } while (cursor.toNextSibling());
 
-                // commit checkpoint: got text runs working at least
-                // planning to trim off option numbers, since the convenient way actually retains them which is
-                // different from the newline way
-                // just check beginning of string against 3 enums for a), 1. i: i guess
+                    // commit checkpoint: got text runs working at least
+                    // planning to trim off option numbers, since the convenient way actually retains them which is
+                    // different from the newline way
+                    // just check beginning of string against 3 enums for a), 1. i: i guess
 
+                    // CTOMathPara might be useful for inline equations
+
+                    // }
+
+
+                }
+
+                // tables
+                else if (element instanceof XWPFTable) {
+                    System.out.println("Table idk: " + ((XWPFTable) element).getText());
+                }
+
+                else {
+                    System.out.println("Type: " + element.getElementType());
+                }
             }
+
+
 
         }
 
