48f6b8ea2581fee0d79b96e04bb2d185374b6a75
waltermcy0110 <waltermcy0110@gmail.com>
2026-06-20 23:37:30 -0400

    handled images, terminal output seems to be in the correct order Next: build html visualisation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,9 @@ package org.example;
 // https://www.baeldung.com/java-microsoft-word-with-apache-poi
 // https://mkyong.com/java/java-read-and-write-microsoft-word-with-apache-poi/
 
+import com.microsoft.schemas.vml.CTImageData;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.IBodyElement;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.*;
 import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlObject;
 import org.openxmlformats.schemas.officeDocument.x2006.math.CTOMath;
@@ -21,6 +19,7 @@ import org.example.EquationParser;
 // import org.openxmlformats.schemas.officeDocument.x2006.math.CTR;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
 // above import is different and results in text runs not getting detected by instanceof CTR
+
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
 
@@ -30,6 +29,9 @@ public class Main {
     static int count;
     XWPFDocument doc;
 
+    // Parsing helpers
+
+
     public static void main(String[] args) throws IOException {
 
         String filename = "dummy_mcq.docx";
@@ -38,34 +40,15 @@ public class Main {
         try (XWPFDocument document = new XWPFDocument(
                 Files.newInputStream(Paths.get(filename)))) {
 
-            List<XWPFParagraph> list = document.getParagraphs();
-
-            // tables are not captured by XWPFParagraph
-            // online sources and https://blog.csdn.net/hwtwilight/article/details/151837532
             for (IBodyElement element: document.getBodyElements()) {
                 // paragraphs: text, equations, etc
                 if (element instanceof XWPFParagraph) {
-                    // oh lmao i kept the original loop
 
-
-                    // for (XWPFParagraph paragraph: list) {
-                    XWPFParagraph paragraph = (XWPFParagraph) element; // just doing this for convenient not readability loll
+                    XWPFParagraph paragraph = (XWPFParagraph) element; // for readability
 
                     // skip empty paragraphs
-                    if (paragraph.getText().trim().isEmpty()) continue;
-
-
-                    // System.out.println("\nParagraph: " + paragraph.getText());
-
-                    // works as expected
-//                        System.out.println("Underlying XML:");
-//                        System.out.println(paragraph.getCTP().xmlText());
-
-                    // parsing of word equations:
-                    // gpt and https://stackoverflow.com/questions/44748712/reading-equations-formula-from-word-docx-to-html-and-save-database-using-jav
-                    // https://developer.adobe.com/experience-manager/reference-materials/cloud-service/javadoc/org/openxmlformats/schemas/wordprocessingml/x2006/main/CTRow.html
-
-
+                    // if (paragraph.runsIsEmpty()) continue;
+                    // commented in case it skips images or sth
 
                     // now iterate over each CTP object
                     XmlCursor cursor = paragraph.getCTP().newCursor();
@@ -77,22 +60,45 @@ public class Main {
                         // System.out.println(xmlobj.xmlText());
 
                         XmlCursor.TokenType tokenType = cursor.currentTokenType();
-                        System.out.println("Type: " + cursor.getName());
+
+                        // System.out.println("Type: " + cursor.getName());
                         // System.out.println("is run? " + (cursor.getObject() instanceof CTR));
                         // runs showed up as non-runs, made me look into import statements
 
                         // determine object type
-                        // text run
-                        if (xmlobj instanceof CTR) {
-                            for (CTText ctText: ((CTR) xmlobj).getTList()) {
-                                System.out.println("Text run: " + ctText.getStringValue());
+                        // Runs (text, images)
+//                        if (xmlobj instanceof CTR) {
+//                            handleCTR(xmlobj);
+//                        }
+
+
+                        // math (inline)
+                        if (xmlobj instanceof CTOMath) {
+                            System.out.println("Equation: " + xmlobj.xmlText());
+                        }
+
+                        // run (might have to do this conversion to preserve ordering if i use xmlobj with eqs)
+                        // credit: "apache poi can i cast ctr to a run"
+                        else if (xmlobj instanceof CTR) {
+                            XWPFRun run = new XWPFRun((CTR)xmlobj, paragraph);
+
+                            // also google ai code, looks fine and non-destructive(?)
+                            // Look for embedded pictures in this specific run
+                            if (!run.getEmbeddedPictures().isEmpty()) {
+                                for (XWPFPicture picture : run.getEmbeddedPictures()) {
+
+                                    // Access metadata and binary source data
+                                    String description = picture.getDescription();
+                                    byte[] rawData = picture.getPictureData().getData();
+
+                                    System.out.println("Found image inline! Description: " + description);
+                                    System.out.println("Image byte size: " + rawData.length);
+                                }
+                            } else {
+                                // Print ordinary text if no image is in the run
+                                System.out.print(run.getText(0));
                             }
 
-                            // TODO: handle images since images are inside runs
-                        }
-                        // math (idk if inline or block)
-                        else if (xmlobj instanceof CTOMath) {
-                            System.out.println("Equation: " + xmlobj.xmlText());
                         }
 
                     } while (cursor.toNextSibling());
@@ -107,7 +113,11 @@ public class Main {
                     // }
 
 
+                    // missing newline after paragraphs
+                    System.out.println("\n");
+
                 }
+
 
                 // tables
                 else if (element instanceof XWPFTable) {
@@ -115,7 +125,7 @@ public class Main {
                 }
 
                 else {
-                    System.out.println("Type: " + element.getElementType());
+                    System.out.println("Other type: " + element.getElementType());
                 }
             }
 
